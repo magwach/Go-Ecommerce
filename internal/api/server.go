c0ad12ea77ed08f21cf444d8b88c6b6a014bdb25
magwach <emmanuelmagothe@gmail.com>
2025-11-04 15:02:26 +0300

    feat: Implemented the user route.

--- internal/api/server.go
+++ internal/api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"go-ecommerce-app/configs"
+	"go-ecommerce-app/internal/api/rest"
+	"go-ecommerce-app/internal/api/rest/controllers"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,5 +11,16 @@ import (
 func StartServer(cfg configs.AppConfig) {
 	port := cfg.ServerPort
 	app := fiber.New()
+
+	rh := &rest.RestHandler{
+		App: app,
+	}
+
+	setupRoutes(rh)
+
 	app.Listen(port)
+}
+
+func setupRoutes(restHand *rest.RestHandler) {
+	controllers.UserRoutes(restHand)
 }
